c4ea008d9c4583c019b1d05e97efe30cd335c35e
Simey de Klerk <simeydeklerk@gmail.com>
2020-05-20 22:43:40 +0200

    tweaks to mega

--- src/pages/mega.js
+++ src/pages/mega.js
@@ -11,6 +11,8 @@ export default function Page() {
     const [activeNum, setActiveNum] = useState(0)
     const [showAnswers, setShowAnswers] = useState(false)
     const toggleShowAnswers = () => setShowAnswers(!showAnswers)
+    const [showWhatsapp, setShowWhatsapp] = useState(false)
+    const toggleShowWhatsapp = () => setShowWhatsapp(!showWhatsapp)
 
     useEffect(() => {
         fetch('/assets/quiz.json').then(res => res.json()).then(setRounds)
@@ -18,24 +20,28 @@ export default function Page() {
 
     const activeRound = ((activeNum >= 0) && (activeNum < rounds.length)) ? rounds[activeNum] : { q: [], a: [] }
     const wat = whatsAppText(activeRound, showAnswers)
-    return <div>
+    return <div class="mega">
         <h1>All the quiz</h1>
         <h2>round {activeNum + 1}</h2>
         <div class="round-button-list">
-            {rounds.map((_, i) => <button class="round-button" onClick={() => setActiveNum(i)}>{i + 1}</button>)}
+            {rounds.map((_, i) => <button class="round-button" onClick={() => {setActiveNum(i); setShowAnswers(false)}}>{i + 1}</button>)}
         </div>
         <div class="vert-list">
         <button class="round-button" onClick={toggleShowAnswers}>{showAnswers ? 'Hide' : 'Show'} Answers</button>
+        <button class="round-button" onClick={toggleShowWhatsapp}>Show {showWhatsapp ? 'Pretty Formatted' : 'Whatsapp Formatted'}</button>
         <Clipboard className="round-button copy-button" data-clipboard-text={wat}>Copy Whatsapp Text to Clipboard</Clipboard>
         </div>
-        <pre>
+        {showWhatsapp ? 
+        <pre className="whatsapp">
             {wat}
         </pre>
-        <ol>
+        :
+        <ol className="pretty">
             {activeRound.q.map((question, i) => <li>
                 <p className="question">{question}</p>
                 {showAnswers ? <p className="answer" ><b>{activeRound.a[i]}</b></p> : ''}
             </li>)}
         </ol>
+        }
     </div>
 }
